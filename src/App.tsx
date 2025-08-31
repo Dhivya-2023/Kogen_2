dc65080d39fd2186d32d2fbf9748a8aa1897e4dc
Dhivya-2023 <dhivyanagaraj2720@gmail.com>
2025-08-31 19:21:04 +0530

    Add the cursor animations

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import SplashCursor from "@/components/ui/SplashCursor"
+
+
 
 const queryClient = new QueryClient();
 
@@ -13,6 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SplashCursor/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
